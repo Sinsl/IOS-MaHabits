604f7ecf5fe33c6b6ca12ed28c36d2fb0abc8369
Sinsl <sinsl@me.com>
2022-05-06 12:20:06 +0300

    Установка иконки приложения, LaunchScreen, начальной сцены и tapBar

---
+++ MyHabits/ExtensionAppColor.swift
@@ -0,0 +1,34 @@
+//
+//  ExtensionAppColor.swift
+//  MyHabits
+//
+//  Created by Mac Home on 06.05.2022.
+//
+
+import Foundation
+import UIKit
+
+public enum NameColor {
+    case grey, purple, blue, green, fiolet, orange
+}
+
+public func AppColor(color: NameColor) -> UIColor {
+    switch color {
+    case .grey:
+        return UIColor(red: 242/255, green: 242/255, blue: 247/255, alpha: 1)
+    case .purple:
+        return UIColor(red: 161/255, green: 22/255, blue: 204/255, alpha: 1)
+    case .blue:
+        return UIColor(red: 41/255, green: 109/255, blue: 255/255, alpha: 1)
+    case .green:
+        return UIColor(red: 29/255, green: 179/255, blue: 34/255, alpha: 1)
+    case .fiolet:
+        return UIColor(red: 98/255, green: 54/255, blue: 255/255, alpha: 1)
+    case .orange:
+        return UIColor(red: 255/255, green: 159/255, blue: 79/255, alpha: 1)
+//    default:
+//        return .systemGray2
+    }
+   
+    
+}
